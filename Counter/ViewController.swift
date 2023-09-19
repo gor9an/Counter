2a7447f1b1e34285456e3909f666b81b2512f99d
gor9an <gordanandrey@yandex.ru>
2023-09-19 21:57:50 +0300

    Added subtraction, reset, history. Cosmetic changes.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -10,22 +10,59 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var counterLabel: UILabel!
-    @IBOutlet weak var counterButton: UIButton!
+    @IBOutlet weak var additionButton: UIButton!
+    @IBOutlet weak var subtractionButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var historyTextView: UITextView!
     private var counter: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         counterLabel.text = "0"
-        counterButton.setTitle("Click", for: .normal)
+        
+        additionButton.setTitle("+", for: .normal)
+        additionButton.layer.cornerRadius = 10
+        
+        subtractionButton.setTitle("-", for: .normal)
+        subtractionButton.layer.cornerRadius = 10
+        
+        resetButton.setTitle("RESET", for: .normal)
+        
+        historyTextView.text = "History:\n"
+        historyTextView.layer.cornerRadius = 10
     }
     
-    @IBAction func buttonDidTap() {
+    @IBAction func additionButtonDidTap() {
         counter += 1
-        counterLabel.text = "Значение счётчика: \(counter)"
+        counterLabel.text = "Counter value: \(counter)"
+        
+        historyTextView.text += "+1\n"
+    }
+    @IBAction func subtractionButtonDidTap() {
+        guard counter != 0 else {
+            historyTextView.text += "0\n"
+            return
+        }
+        
+        if counter - 1 > 0 {
+            counter -= 1
+            counterLabel.text = "Counter value: \(counter)"
+        } else {
+            counter = 0
+            counterLabel.text = "0"
+        }
+        
+        historyTextView.text += "-1\n"
     }
     
-
+    @IBAction func resetButtonDidTap() {
+        counter = 0
+        counterLabel.text = "0"
+        
+        historyTextView.text += "reset\n"
+    }
+    
 
 }
 
